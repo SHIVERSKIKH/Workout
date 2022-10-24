79211ce404b59473499a335b6c8cff73ce707c57
shiverskikh <katerina_kononenko_94@mail.ru>
2022-10-24 15:45:00 +0700

    Add tasks

--- Workout_01/Program.cs
+++ Workout_01/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 class PleaseSayUra
 {
     static void Main()
@@ -7,3 +7,56 @@ class PleaseSayUra
         Console.ReadLine();
     }
 }
+*/
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*void Comparison(int a, int b)
+{
+    if(a > b)
+    {
+        Console.WriteLine($"Max number {a}");
+    }
+    else
+    {
+        Console.WriteLine($"Max number {b}");
+    }
+}
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Comparison(n1, n2);
+*/
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*int Comparison(int a, int b, int c)
+{
+    int max = a;
+    if(b > max) max = b;
+    if(c > max) max = c;
+    return max;
+}
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int res = Comparison(n1, n2, n3);
+Console.WriteLine($"Your max number {res}");
+*/
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*bool Examination(int num)
+{
+    if(num % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Console.Write("Input your number: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+bool res = Examination(Num);
+Console.WriteLine(res);
+*/
