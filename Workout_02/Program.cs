707470a6458601e8453a6d4a56cba503a023f26e
shiverskikh <katerina_kononenko_94@mail.ru>
2022-10-26 21:49:48 +0700

    Added  workout 02

---
+++ Workout_02/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*double Work(double a, double b)
+{
+    double work = Math.Pow(a, b);
+    return work;
+}
+Console.WriteLine("Input a first number: ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a second number: ");
+double n2 = Convert.ToInt32(Console.ReadLine());
+double res = Work(n, n2);
+Console.WriteLine($"Yoyr result {res}");
+*/
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
