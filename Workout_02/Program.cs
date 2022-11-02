ce3cb0abfb2938e5a08e95e637fc5aaf9cf33557
shiverskikh <katerina_kononenko_94@mail.ru>
2022-11-02 14:57:50 +0700

    Added  task 3

--- Workout_02/Program.cs
+++ Workout_02/Program.cs
@@ -28,6 +28,28 @@ int num = Convert.ToInt32(Console.ReadLine());
 int res = Sum(num);
 Console.WriteLine($"Your sum number {res}");
 */
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33 -> [6, 1, 33]
+/*int[] array = new int[8];
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите число : ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива:  ");
+Console.Write("[");
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + ",");
+}
+Console.Write("]");
+*/
+
+
+
 
 
 
